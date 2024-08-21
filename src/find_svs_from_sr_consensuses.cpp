f038a148c0e6aa0f2867d61e61b1f724a882f6de
ramesh <ramesh.rajaby@gmail.com>
2024-08-21 13:59:12 +0000

    Improve inversions precision.

--- src/find_svs_from_sr_consensuses.cpp
+++ src/find_svs_from_sr_consensuses.cpp
@@ -352,7 +352,8 @@ void find_indels_from_rc_lc_pairs(std::string contig_name, std::vector<consensus
 		std::vector<std::tuple<hts_pos_t, breakend_t*, breakend_t*>> bnd_pairs;
 		for (breakend_t* bnd_rf : bnds_rf) {
 			for (breakend_t* bnd_lf : bnds_lf) {
-				if (std::abs(bnd_rf->start-bnd_lf->start) <= stats.max_is && std::abs(bnd_rf->end-bnd_lf->end) <= stats.max_is) {
+				if (bnd_lf->start-bnd_rf->start <= stats.max_is && bnd_lf->start-bnd_rf->start > -stats.read_len &&
+					bnd_lf->end-bnd_rf->end <= stats.max_is && bnd_lf->end-bnd_rf->end > -stats.read_len) {
 					hts_pos_t dist = std::abs(bnd_rf->start-bnd_lf->start) + std::abs(bnd_rf->end-bnd_lf->end);
 					bnd_pairs.push_back(std::make_tuple(dist, bnd_rf, bnd_lf));
 				}
