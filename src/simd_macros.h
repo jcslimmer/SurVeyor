9d7a9b3f7ae990e90fc0c7b193a0eed26727f8b7
Mesh89 <mesh89@gmail.com>
2026-04-02 18:31:26 +0800

    Added NEON support.

--- src/simd_macros.h
+++ src/simd_macros.h
@@ -5,12 +5,14 @@
 #include <cstring>
 #include <algorithm>
 
-#if !defined(USE_SCALAR) && !defined(USE_SSE) && !defined(USE_AVX2) //&& !defined(USE_AVX512)
+#if !defined(USE_SCALAR) && !defined(USE_SSE) && !defined(USE_AVX2) && !defined(USE_NEON) //&& !defined(USE_AVX512)
   // #if defined(__AVX512F__) && defined(__AVX512BW__)  CURRENTLY I HAVE NO WAY TO TEST THIS AS I HAVE NO ACCESS TO AVX512 HARDWARE
     // AVX512BW is required for byte/word (16-bit) operations
     // #define USE_AVX512
   #if defined(__AVX2__)
     #define USE_AVX2
+  #elif defined(__ARM_NEON) || defined(__ARM_NEON__)
+    #define USE_NEON
   #elif defined(__SSE4_2__)
     #define USE_SSE
   #else
@@ -56,6 +58,58 @@ static inline void store_int_unaligned_16(void* p, SIMD_INT_16 v) {
 
 #define INT_PER_BLOCK_16 1
 #define BYTES_PER_BLOCK_16 (sizeof(SIMD_INT_16))
+
+#elif defined(USE_NEON)
+#include <arm_neon.h>
+
+typedef int16x8_t SIMD_INT_16;
+
+static inline SIMD_INT_16 load_int_16_neon(const void* p) {
+    return vreinterpretq_s16_u8(vld1q_u8(reinterpret_cast<const uint8_t*>(p)));
+}
+
+static inline void store_int_16_neon(void* p, SIMD_INT_16 v) {
+    vst1q_u8(reinterpret_cast<uint8_t*>(p), vreinterpretq_u8_s16(v));
+}
+
+static inline int count_equal_bytes_16_neon(SIMD_INT_16 a, SIMD_INT_16 b) {
+    uint8x16_t eq = vceqq_u8(vreinterpretq_u8_s16(a), vreinterpretq_u8_s16(b));
+    uint8x16_t ones = vshrq_n_u8(eq, 7);
+    uint16x8_t sum16 = vpaddlq_u8(ones);
+    uint32x4_t sum32 = vpaddlq_u16(sum16);
+    uint64x2_t sum64 = vpaddlq_u32(sum32);
+    return (int)(vgetq_lane_u64(sum64, 0) + vgetq_lane_u64(sum64, 1));
+}
+
+static inline int cmp_gt_int_16_neon(SIMD_INT_16 a, SIMD_INT_16 b) {
+    uint64x2_t cmp = vreinterpretq_u64_u16(vcgtq_s16(a, b));
+    return (vgetq_lane_u64(cmp, 0) | vgetq_lane_u64(cmp, 1)) != 0;
+}
+
+static inline int cmp_gt_int_16_except_last_neon(SIMD_INT_16 a, SIMD_INT_16 b) {
+    uint16x8_t cmp = vcgtq_s16(a, b);
+    cmp = vsetq_lane_u16(0, cmp, 7);
+    uint64x2_t cmp64 = vreinterpretq_u64_u16(cmp);
+    return (vgetq_lane_u64(cmp64, 0) | vgetq_lane_u64(cmp64, 1)) != 0;
+}
+
+#define SET1_INT_16 vdupq_n_s16
+#define LOAD_INT_16(p) load_int_16_neon(p)
+#define STORE_INT_16(p, v) store_int_16_neon((p), (v))
+#define LOADU_INT_16(p) load_int_16_neon(p)
+#define STOREU_INT_16(p, v) store_int_16_neon((p), (v))
+
+#define ADD_INT_16 vaddq_s16
+#define ADDS_INT_16 vqaddq_s16 // Saturated Add
+
+#define MAX_INT_16 vmaxq_s16
+#define CMP_GT_INT_16(a, b) cmp_gt_int_16_neon((a), (b))
+#define CMP_GT_INT_16_EXCEPT_LAST(a, b) cmp_gt_int_16_except_last_neon((a), (b))
+
+#define COUNT_EQUAL_BYTES_16(a, b) count_equal_bytes_16_neon((a), (b))
+
+#define INT_PER_BLOCK_16 8
+#define BYTES_PER_BLOCK_16 16
 
 #elif defined(USE_SSE)
 #include <immintrin.h>
@@ -132,7 +186,7 @@ typedef __m512i SIMD_INT_16;
 #define BYTES_PER_BLOCK_16 64
 
 #else
-#error "Either USE_SCALAR, USE_SSE, USE_AVX2, or USE_AVX512 must be defined"
+#error "Either USE_SCALAR, USE_NEON, USE_SSE, USE_AVX2, or USE_AVX512 must be defined"
 #endif
 
 #endif /* SIMD_MACROS_H_ */
